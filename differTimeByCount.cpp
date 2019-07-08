d5a9080287522858704709ff5013748dfad8dba4
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2019-07-09 02:01:26 +0700

    Create differTimeByCount.cpp
    Use this function to calculate the difference in time using millis() or micros() Arduino.

---
+++ differTimeByCount.cpp
@@ -0,0 +1,13 @@
+// This function calculating the difference in time using timer counter in millis() or micros() Arduino.
+uint32_t getDifference(uint32_t& t0, uint32_t& t1) {
+	uint32_t returnValue = 0;
+	if (t1 > t0) {
+		returnValue = t1 - t0;
+		return returnValue;
+	}
+	else {
+		uint32_t x = ((2 ^ 32) - 1) - t0;
+		returnValue = t1 + x;
+		return returnValue;
+	}
+}
