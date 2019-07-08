909f86dd157c5a2d59b19286fa5607a5338799a1
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2019-07-09 02:06:27 +0700

    Update differTimeByCount.cpp

--- differTimeByCount.cpp
+++ differTimeByCount.cpp
@@ -1,5 +1,5 @@
 // This function calculating the difference in time using timer counter in millis() or micros() Arduino.
-uint32_t getDifference(uint32_t& t0, uint32_t& t1) {
+uint32_t getDifference(uint32_t t0, uint32_t t1) {
 	uint32_t returnValue = 0;
 	if (t1 > t0) {
 		returnValue = t1 - t0;
