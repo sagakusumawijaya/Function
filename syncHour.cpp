4ff28f1a8c3456e1fa28e5ba335b6baa161608c4
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2020-04-29 00:40:53 +0700

    Update syncHour.cpp

--- syncHour.cpp
+++ syncHour.cpp
@@ -3,10 +3,10 @@
  * @details		Jika waktu telah menununjukkan lebih dari 24, maka akan disesuaikan
  *				dengan format waktu 24 jam.
  * @param1 [in]	hour	: Waktu dalam jam (0-23)
- * @param2 [in] gmt		: Nilai GMT (0-12)
+ * @param2 [in] gmt	: Nilai GMT (0-12)
  * @param3 [in] gmtSign	: Tanda GMT ( + atau - )
  * @return		Waktu yang telah terformat.
- *				-1 jika salam memasukkan parameter 'gmt' dan 'gmtSign'
+ *				-1 jika salah memasukkan parameter 'gmt' dan 'gmtSign'
  */
 int8_t syncHour(int8_t hour, int8_t gmt = 0, char gmtSign = '+') {
 	if (hour > 23 || hour < 0)
